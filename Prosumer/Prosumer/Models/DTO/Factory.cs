dca76cf2d09dc79095af3ca64249f3cece89a335
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2018-12-13 16:29:32 +0100

    some more work on DTO almost done with full DTO

--- Prosumer/Prosumer/Models/DTO/Factory.cs
+++ Prosumer/Prosumer/Models/DTO/Factory.cs
@@ -2,39 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProsumerInfo.Models.DTOinterface;
 
 namespace ProsumerInfo.Models.DTO
 {
-    public class Factory
+    public class Factory: IFactory
     {
-        public ProsumerConsumptionDTO CreateProsumerConsumptionDTO(ProsumerConsumption prosumerC)
+        public ProsumerConsumptionFullDTO CreateProsumerConsumptionDTO(ProsumerConsumption prosumerC)
         {
-            var dto = new ProsumerConsumptionDTO()
+            var dto = new ProsumerConsumptionFullDTO()
             {
                 id = prosumerC.id,
                 PC_Timestamp = prosumerC.PC_Timestamp,
                 ProsumerBalance = prosumerC.ProsumerBalance,
 
-                _prosumer = new Prosumer
+                Prosumer = new ProsumerDTO
                 {
-                    ProsumerType = prosumerC._prosumer.ProsumerType,
+                    prosumerType = prosumerC._prosumer.ProsumerType,
                     X10_ID = prosumerC._prosumer.X10_ID,
 
-                    consumption = new Consumption
+                    consumption = new ConsumptionDTO
                     {
                         KWH = prosumerC._prosumer.consumption.KWH,
-                        Consumption_time = prosumerC._prosumer.consumption.Consumption_time
+                        ConsumptionTime = prosumerC._prosumer.consumption.Consumption_time
                     },
 
-                    production = new Production
+                    production = new ProductionDTO
                     {
                         KWH = prosumerC._prosumer.production.KWH,
                         ProductionTime = prosumerC._prosumer.production.ProductionTime
                     }
-                };
-                return dto;
+                }
+            };
+        return dto;
         }
-            
+
+        public ConsumptionDTO CreateSingleConsumptionDTO(Consumption consumption)
+        {
+            var dto = new ConsumptionDTO
+            {
+                KWH = consumption.KWH,
+                ConsumptionTime = consumption.Consumption_time
+            };
+            return dto;
+        }
+
+        public ProductionDTO CreateSingleProductionDTO(Production production)
+        {
+
+            var dto = new ProductionDTO
+            {
+                KWH = production.KWH,
+                ProductionTime = production.ProductionTime
+            };
+            return dto;
+        }
+
+        public ProsumerDTO CreateSingleProsumerDTO(Prosumer prosumer)
+        {
+            var dto = new ProsumerDTO
+            {
+                prosumerType = prosumer.ProsumerType,
+                X10_ID = prosumer.X10_ID,
+
+                consumption = new ConsumptionDTO
+                {
+                    KWH = prosumer.consumption.KWH,
+                    ConsumptionTime = prosumer.consumption.Consumption_time
+                },
+
+                production = new ProductionDTO
+                {
+                    KWH = prosumer.production.KWH,
+                    ProductionTime = prosumer.production.ProductionTime
+                }
+            };
+            return dto;
         }
     }
 }
---
+++ Prosumer/Prosumer/Models/DTOinterface/IFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProsumerInfo.Models;
+using ProsumerInfo.Models.DTO;
+
+
+namespace ProsumerInfo.Models.DTOinterface
+{
+    interface IFactory
+    {
+        ProsumerConsumptionDTO CreateProsumerConsumptionDTO(ProsumerConsumption prosumerconsumption);
+        ProsumerDTO CreateSingleProsumerDTO(Prosumer prosumer);
+        ProductionDTO CreateSingleProductionDTO(Production production);
+        ConsumptionDTO CreateSingleConsumptionDTO(Consumption consumption);
+       
+    }
+}
